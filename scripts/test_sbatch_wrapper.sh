463deba886ee5fec7adc5953e1d3998edb4b4bc7
sarah-barr <eeslb@host596.jc.rl.ac.uk>
2021-04-19 12:32:20 +0100

    added scripts directory

---
+++ scripts/test_sbatch_wrapper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+job_directory=$PWD/plot_all_test
+job_name=salalah_test
+DATA_DIR=/gws/nopw/j04/ncas_radar_vol2/pestdar/oman/salalah/raw/cfradial/
+
+DATES=(${DATA_DIR}/*)
+
+ARRAY_END=$((${#DATES[@]}-1))
+
+JOB_FILE="${job_directory}/test_array.sbatch"
+
+	echo "#!/bin/bash
+#SBATCH --partition=short-serial
+#SBATCH --job-name=${job_name}
+#SBATCH --output=%A_%a.out
+#SBATCH --error=%A_%a.err
+#SBATCH --time=5:00
+#SBATCH --array=0-${ARRAY_END}
+
+DATA_DIR=${DATA_DIR}
+
+DATES=(\${DATA_DIR}/*)
+
+DATE_PATH=\${DATES[\$SLURM_ARRAY_TASK_ID]}
+
+source activate /home/users/eeslb/.bashrc
+source activate /home/users/eeslb/miniconda3/envs/pyart_3_8_biodar
+
+python plot_radar_all.py \${DATE_PATH} 1 100 /gws/nopw/j04/ncas_radar_vol1/eeslb/pestdar/plots/oman/salalah " > $JOB_FILE
